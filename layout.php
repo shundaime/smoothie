aedf8450f062e19cf27f95bc9ed283c91ca5d854
BadissSaysHello <a.badiss@hotmail.com>
2018-12-18 10:23:44 +0100

    css melissa

--- layout.php
+++ layout.php
@@ -5,6 +5,7 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+		<link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
 		<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" media="all" />
@@ -15,143 +16,78 @@
 	<body>
 		<!--HEADER-->
 		<header>
-		<a href="#"><img src="http://placehold.it/90x90"></a>
+            <a href="#"><img src="assets/logo/desktop-logo.png"></a>
 			<nav>
 				<ul>
-					<li><a href="#">A propos</a></li>
+					<li><a href="#">Nos produit</a></li>
 					<li><a href="#">Notre concept</a></li>
-					<li><a href="#">Nos smoothie</a></li>
-					<li><a href="#">Les recettes</a></li>
-					<li><a href="#">Contact</a></li>
+					<li><a href="#">Nous contacter</a></li>
+					<li><a href="#">Connectez-vous</a></li>
 				</ul>
             </nav>
-            <div id="connexion">
-                <ul>
-					<li><a href="#"><i class="far fa-user"></i>Espace personnel</a></li>
-					<li><a href="#"><i class="far fa-user"></i>Se déconnecter</a></li>
-                </ul>
-            </div>
-
 		</header>
 		<!--MAIN CONTENT-->
 		<main>
+			<h1>Local, Bio et éco-responsable</h1>
+            <h2>Nos petits derniers</h2>
+            <section class="last-products">
+                <article>
+                    <ul>
+                        <li><a href="#"><img src="assets/img_produit/nos-jus-zen.jpg"></a></li>
+                        <li><a href="#"><h3>Smoothie Zen</h3></a></li>
+                        <li><a href="#"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi eius sunt, aliquam culpa iusto libero.</p></a></li>
+                    </ul>
+                </article>
+                <article>
+                    <ul>
+                        <li><a href="#"><img src="assets/img_produit/nos-jus-pulse.jpg"></a></li>
+                        <li><a href="#"><h3>Smoothie Pulse</h3></a></li>
+                        <li><a href="#"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi eius sunt, aliquam culpa iusto libero.</p></a></li>
+                    </ul>
+                </article>
+                <article>
+                    <ul>
+                        <li><a href="#"><img src="assets/img_produit/nos-jus-young.jpg"></a></li>
+                        <li><a href="#"><h3>Smoothie Young</h3></a></li>
+                        <li><a href="#"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi eius sunt, aliquam culpa iusto libero.</p></a></li>
+                    </ul>
+                </article>
+            </section>
 
-				 <section id="myServices">
-                    <h1><a href="#">Services</a></h1>
-                    <div class="serviceContainer">
-                    <div class="services">
-                        <ul>
-                            <li><a href="#"><img src="http://placehold.it/200x200"></a></li>
-                            <li><a href="#"><h2>Bio</h2></a></li>
-                            <li><p>Sed (saepe enim redeo ad Scipionem, cuius omnis sermo erat de amicitia) querebatur, quod omnibus in rebus homines diligentiores essent; capras et oves quot quisque haberet, dicere posse, amicos quot haberet.</p></li>
-                            <li><a href="#"><p class="clickUp">En savoir plus !</p></a></li>
-                        </ul>
-                    </div>
-                    <div class="services">
-                        <ul>
-                            <li><a href="#"><img src="http://placehold.it/200x200"></a></li>
-                            <li><a href="#"><h2>Eco-responsable</h2></a></li>
-                            <li><p>Sed (saepe enim redeo ad Scipionem, cuius omnis sermo erat de amicitia) querebatur, quod omnibus in rebus homines diligentiores essent; capras et oves quot quisque haberet, dicere posse.</p></li>
-                            <li><a href="#"><p class="clickUp">En savoir plus !</p></a></li>
-                        </ul>
-                    </div>
-                    <div class="services">
-                            <ul>
-                                <li><a href="#"><img src="http://placehold.it/200x200"></a></li>
-                                <li><a href="#"><h2>Santé</h2></a></li>
-                                <li><p>Sed (saepe enim redeo ad Scipionem, cuius omnis sermo erat de amicitia) querebatur, quod omnibus in rebus homines diligentiores essent; capras et oves quot quisque haberet, dicere posse, amicos quot haberet, non posse dicere et in illis quidem parandis .</p></li>
-                                <li><a href="#"><p class="clickUp">En savoir plus !</p></a></li>
-                            </ul>
-                        </div>
-                        </div>
-                </section>
-
-				<section id="promotion">
-					<h1><a href="#">Du nouveau par ici!</a></h1>
-                    <div class="promotionContent">
-                        <ul>
-                            <li><a href="#"><h3>Smoothie du jour</h3></a></li>
-                            <li><a href="#"><img src="http://placehold.it/200x200"></a></li>
-                            <li><p>Cuius generis est magna penuria. Et iudicare difficile est sane nisi expertum; experiendum autem est in ipsa amicitia. Ita praecurrit amicitia iudicium tollitque experiendi potestatem.</p></li>
-							<li><a href="#"><p class="clickUp">Voir les smoothies!</p></a></li>
-                        </ul>
-                    </div>
-                    <div class="promotionContent">
-                        <ul>
-                            <li><a href="#"><h3>Créez votre recette !</h3></a></li>
-                            <li><a href="#"><img src="http://placehold.it/200x200"></a></li>
-                            <li><p>Cuius generis est magna penuria. Et iudicare difficile est sane nisi expertum; experiendum autem est in ipsa amicitia. Ita praecurrit amicitia iudicium tollitque experiendi potestatem.</p></li>
-                            <li><a href="#"><p class="clickUp">C'est parti!</p></a></li>
-                        </ul>
-                    </div>
-                </section>
-
-				 <section id="products">
-				 	<h1><a href="#">Nos derniers smoothies</a></h1>
-                    <div class="oneProduct">
-                        <ul>
-                            <li><a href="#"><img src="http://placehold.it/200x200"></li>
-                            <li><h4>Pure Bliss</h4></li>
-                            <li><p>4,90€</p></li>
-                            <li><p class="clickUp">Voir</p></li>
-                        </ul>
-                    </div>
-                    <div class="oneProduct">
-                            <ul>
-                                <li><a href="#"><img src="http://placehold.it/200x200"></li>
-                                <li><h4>Pure Bliss</h4></li>
-                                <li><p>4,90€</p></li>
-                                <li><p class="clickUp">Voir</p></li>
-                            </ul>
-                        </div>
-                        <div class="oneProduct">
-                            <ul>
-                                <li><a href="#"><img src="http://placehold.it/200x200"></li>
-                                <li><h4>Pure Bliss</h4></li>
-                                <li><p>4,90€</p></li>
-                                <li><p class="clickUp">voir</p></li>
-                            </ul>
-                        </div>
-                </section>
-
-				 <section id="articles">
-				 	<h1><a href="#">Les dernières recettes</a></h1>
-                    <article class="articleSubstr">
-                        <ul>
-                            <li><h2>Recette 1</h2></li>
-                            <li><img src="http://placehold.it/200x200"></li>
-                            <li><p>Sed (saepe enim redeo ad Scipionem, cuius omnis sermo erat de amicitia) querebatur, quod omnibus in rebus homines diligentiores essent; capras et oves quot quisque haberet, dicere posse, amicos quot haberet...</p></li>
-                            <li><a href="#"><p class="clickUp">Lire l'article</p></a></li>
-                        </ul>
-                    </article>
-                    <article class="articleSubstr">
-                        <ul>
-                            <li><h2>Recette 2</h2></li>
-                            <li><img src="http://placehold.it/200x200"></li>
-                            <li><p>Sed (saepe enim redeo ad Scipionem, cuius omnis sermo erat de amicitia) querebatur, quod omnibus in rebus homines diligentiores essent; capras et oves quot quisque haberet, dicere posse, amicos quot haberet...</p></li>
-                            <li><a href="#"><p class="clickUp">Lire l'article</p></a></li>
-                        </ul>
-                    </article>
-                    <article class="articleSubstr">
-                        <ul>
-                            <li><h2>Recette 3</h2></li>
-                            <li><img src="http://placehold.it/200x200"></li>
-                            <li><p>Sed (saepe enim redeo ad Scipionem, cuius omnis sermo erat de amicitia) querebatur, quod omnibus in rebus homines diligentiores essent; capras et oves quot quisque haberet, dicere posse, amicos quot haberet...</p></li>
-                            <li><a href="#"><p class="clickUp">Lire l'article</p></a></li>
-                        </ul>
-                    </article>
-                </section>
-
+            <section class="categories">
+            <article>
+                    <ul>
+                        <li><a href="#"><img src="assets/iconVincent/fruit.png"></a></li>
+                        <li><a href="#"><h3>Fruits</h3></a></li>
+                        <li><a href="#"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi eius sunt, aliquam culpa iusto libero.</p></a></li>
+                    </ul>
+                </article>
+                <article>
+                    <ul>
+                        <li><a href="#"><img src="assets/iconVincent/legume.png"></a></li>
+                        <li><a href="#"><h3>Légumes</h3></a></li>
+                        <li><a href="#"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi eius sunt, aliquam culpa iusto libero.</p></a></li>
+                    </ul>
+                </article>
+                <article>
+                    <ul>
+                        <li><a href="#"><img src="assets/iconVincent/sport.png"></a></li>
+                        <li><a href="#"><h3>Santé</h3></a></li>
+                        <li><a href="#"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi eius sunt, aliquam culpa iusto libero.</p></a></li>
+                    </ul>
+                </article>
+            </section>
+          
 		</main>
 		<!--FOOTER-->
 		<footer>
 		<div id="footerLogo">
             <a href="#">
-			<img src="http://placehold.it/80x80">
+			<img src="assets/logo/desktop-logo.png">
             </a>
         </div>
         <div class="footerText">
-            <p>2018 &copy; Tous droits réservés Vincent, Mélanie, Guillaume et Badiss</p>
+            <p>2018 &copy; Tous droits réservés Vincent, Melissa aka Mel, Guillaume et Badiss</p>
         </div>
 			
 		</footer>
