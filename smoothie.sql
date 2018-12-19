64c95d55fa32b76fdb1d136eddc9cd7eaaa49805
twenty-cent <prudh.vin.cent@gmail.com>
2018-12-19 11:51:04 +0100

    Dernière Version BDD avant la prochaine

--- smoothie.sql
+++ smoothie.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Mar 18 Décembre 2018 à 11:06
+-- Généré le :  Mer 19 Décembre 2018 à 11:43
 -- Version du serveur :  5.7.24-0ubuntu0.16.04.1
 -- Version de PHP :  7.0.32-0ubuntu0.16.04.1
 
@@ -28,17 +28,18 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `categorie` (
   `id` int(5) NOT NULL,
-  `libelle` varchar(30) NOT NULL
+  `libelle` varchar(30) NOT NULL,
+  `picture` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `categorie`
 --
 
-INSERT INTO `categorie` (`id`, `libelle`) VALUES
-(1, 'fruit'),
-(2, 'legume'),
-(3, 'sport');
+INSERT INTO `categorie` (`id`, `libelle`, `picture`) VALUES
+(1, 'fruit', 'fruit.png'),
+(2, 'legume', 'legume.png'),
+(3, 'sport', 'sport.png');
 
 -- --------------------------------------------------------
 
@@ -55,6 +56,25 @@ CREATE TABLE `comment` (
   `idUser` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Contenu de la table `comment`
+--
+
+INSERT INTO `comment` (`id`, `pseudo`, `content`, `dateCreation`, `idProduct`, `idUser`) VALUES
+(2, 'oui', 'waw', '2018-12-18 15:14:10', 2, 5),
+(3, 'blblblbl', 'SMOOTHIE', '2018-12-18 15:14:48', 4, 2),
+(4, 'cc', 'cv', '2018-12-18 15:15:47', 1, 5),
+(5, 'Michel', 'cool', '2018-12-18 16:09:15', 6, 10),
+(6, 'Sandrine ', 'je suis zen', '2018-12-18 16:10:07', 1, 10),
+(7, 'bonsoir', 'bonsoir', '2018-12-18 16:11:09', 1, 9),
+(8, 'JPdu77', 'très frais.', '2018-12-18 16:11:52', 3, 9),
+(9, 'RouxArriere', 'feur lol', '2018-12-18 16:12:44', 6, 8),
+(10, 'MichelMichel', 'coolcool', '2018-12-18 16:13:12', 4, 8),
+(11, 'Mfczeiofj', 'f,ckzlevn,oefc,lz', '2018-12-18 16:15:28', 5, 6),
+(12, 'A', 'tartiflette', '2018-12-18 16:16:13', 5, 7),
+(13, 'blblblb', 'blblblblblb', '2018-12-18 16:17:09', 2, 7),
+(14, 'Gandalf?', 'hey', '2018-12-18 16:18:03', 3, 6);
+
 -- --------------------------------------------------------
 
 --
@@ -65,6 +85,32 @@ CREATE TABLE `favorite` (
   `idUser` int(11) NOT NULL,
   `idReceipe` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `favorite`
+--
+
+INSERT INTO `favorite` (`idUser`, `idReceipe`) VALUES
+(2, 3),
+(4, 5),
+(10, 6),
+(9, 5),
+(10, 1),
+(10, 2),
+(9, 2),
+(8, 1),
+(8, 5),
+(8, 2),
+(7, 4),
+(7, 1),
+(7, 6),
+(6, 2),
+(6, 6),
+(6, 5),
+(5, 6),
+(5, 4),
+(5, 1),
+(4, 4);
 
 -- --------------------------------------------------------
 
@@ -87,12 +133,12 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `nameProduct`, `picture`, `description`, `ingredients`, `dateCreation`, `idCategorie`) VALUES
-(1, 'zen', '<img src="assets/photos/nos-jus-zen.jpg>', 'pomme, épinard, kale, carotte, citron, estragon, gingembre', '', '2018-12-17 11:04:00', 3),
-(2, 'beautiful', '<img src="assets/photos/nos-jus-beautiful.jpg>', 'raisin, carotte, betterave, céleri, citron', '', '2018-12-18 11:04:00', 2),
-(3, 'young', '<img src="assets/photos/nos-jus-young.jpg">', 'carotte, pomme, fenouil, navet, jus d\'aloe vera, raisin', '', '2018-12-16 11:04:00', 3),
-(4, 'good', '<img src="assets/photos/nos-jus-good.jpg">', 'concombre, kiwi, pomme, brocoli, menthe, citron', '', '2018-12-15 11:04:00', 2),
-(5, 'pulse', '<img src="assets/photos/nos-jus-pulse.jpg">', 'betterave, carotte, pomme, citron, gingembre', '', '2018-12-14 11:04:00', 1),
-(6, 'detox', '<img src="assets/photos/nos-jus-detox.jpg">', 'pomme, concombre, fenouil, épinard, citron, céleri', '', '2018-12-13 11:04:00', 1);
+(1, 'zen', 'nos-jus-zen.jpg', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi eius sunt, aliquam culpa iusto libero.', 'pomme, épinard, kale, carotte, citron, estragon, gingembre', '2018-12-17 11:04:00', 3),
+(2, 'beautiful', 'nos-jus-beautiful.jpg', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi eius sunt, aliquam culpa iusto libero.', 'raisin, carotte, betterave, céleri, citron', '2018-12-18 11:04:00', 2),
+(3, 'young', 'nos-jus-young.jpg', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi eius sunt, aliquam culpa iusto libero.', 'carotte, pomme, fenouil, navet, jus d\'aloe vera, raisin', '2018-12-16 11:04:00', 3),
+(4, 'good', 'nos-jus-good.jpg', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi eius sunt, aliquam culpa iusto libero.', 'concombre, kiwi, pomme, brocoli, menthe, citron', '2018-12-15 11:04:00', 2),
+(5, 'pulse', 'nos-jus-pulse.jpg', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi eius sunt, aliquam culpa iusto libero.', 'betterave, carotte, pomme, citron, gingembre', '2018-12-14 11:04:00', 1),
+(6, 'detox', 'nos-jus-detox.jpg', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi eius sunt, aliquam culpa iusto libero.', 'pomme, concombre, fenouil, épinard, citron, céleri', '2018-12-13 11:04:00', 1);
 
 -- --------------------------------------------------------
 
@@ -112,7 +158,11 @@ CREATE TABLE `receipe` (
 
 INSERT INTO `receipe` (`id`, `details`, `idProduct`) VALUES
 (1, 'et un peu de sucre en poudre', 1),
-(2, 'mixer le tout servez', 4);
+(2, 'mixer le tout servez', 4),
+(3, 'eplucher mixez manber', 2),
+(4, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut ipsum magna. Aenean vel laoreet dui, ac tristique felis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In metus tellus, auctor sed tempor id, malesuada nec tortor.', 3),
+(5, ' Aliquam in est eros. Sed id commodo erat. In vel eros sapien. Cras sed iaculis nisl, id ornare orci. Nullam est arcu, elementum quis pharetra vitae, molestie et nisl.', 6),
+(6, ' Aliquam in est eros. Sed id commodo erat. In vel eros sapien. Cras sed iaculis nisl, id ornare orci. Nullam est arcu, elementum quis pharetra vitae, molestie et nisl.', 5);
 
 -- --------------------------------------------------------
 
@@ -139,8 +189,13 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `mail`, `pseudo`, `password`, `firstName`, `name`, `address`, `zipCode`, `city`, `Creation_time_stamp`) VALUES
 (2, 'gcharmassonjm@gmail.com', 'Perceval', '123456', 'Guillaume', 'Guillaume Charmasson', 'Lieu dit Lioriac, 48 rue des vignobles', '43590', 'Beauzac', '2018-12-17 15:27:12'),
-(4, 'gcharmassonjm@gmail.com', 'dfsqdf', '123456', 'Guillaume', 'Guillaume Charmasson', 'Lieu dit Lioriac, 48 rue des vignobles', '43590', 'Beauzac', '2018-12-17 15:40:58'),
-(5, 'gcharmassonjm@gmail.com', 'shundaime', 'azerty', 'Guillaume', 'Guillaume Charmasson', 'Lieu dit Lioriac, 48 rue des vignobles', '43590', 'Beauzac', '2018-12-18 08:07:05');
+(4, 'AntonioDeLaStreet@gmail.com', 'ggggggg', 'narutoLPB', 'Antonio', 'DeLaStreet', 'A la street', '58110', 'Cambera', '2018-12-19 08:35:00'),
+(5, 'strvalentin@hotmail.fr', 'str', 'gateau01', 'str', 'Valentin', '14 rue ghiozujpoz', '01221', 'Lille', '2018-12-19 08:33:14'),
+(6, 'coca55@gmail.com', 'oeoe', 'password02', 'coca', 'cola', '12 hufbrez vfd', '45100', 'toulouse', '2018-12-18 14:42:09'),
+(7, 'micheltv@outlook.fr', 'Michel', '1945', 'tkv', 'Michel', '55 hvu vureizu', '58442', 'lille', '2018-12-18 14:45:02'),
+(8, 'LALALALALA@gmail.fr', 'lala', 'mdplal', 'LALA', 'laLA', '01 iozvhbjie fdud', '47842', 'Grenoble', '2018-12-18 14:47:36'),
+(9, 'smoothie@smoothie.fr', 'smoothie', '5866', 'smoo', 'thie', '59 vjioedbij hdio', '24657', 'Strasbourg', '2018-12-18 14:50:39'),
+(10, 'coquillagerose@gmail.com', 'heu', 'azerty', 'Brigitte', 'Bardot', '33 vzi hize czi', '1423', 'Montpellier', '2018-12-18 14:53:37');
 
 --
 -- Index pour les tables exportées
@@ -200,7 +255,7 @@ ALTER TABLE `categorie`
 -- AUTO_INCREMENT pour la table `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT pour la table `product`
 --
@@ -210,12 +265,12 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT pour la table `receipe`
 --
 ALTER TABLE `receipe`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- Contraintes pour les tables exportées
 --
